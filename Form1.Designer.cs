89bff79733cc2282fbb6544cd67b5fcc149b8d22
Dev Trainner <Paulo.bahia@playlist.com.br>
2023-07-14 09:57:26 -0300

    Implementações Iniciais Ler XML
    Foi implementado a leitura do arquivo XML Montagem.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LeituraXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(787, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ler XML";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.RowTemplate.Height = 25;
+            this.dataGrid1.Size = new System.Drawing.Size(787, 394);
+            this.dataGrid1.TabIndex = 3;
+            this.dataGrid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button button1;
+        private DataGridView dataGrid1;
+    }
+}
---
+++ Utils/Playlist.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+namespace LeituraXml.Utils
+{
+    public class Playlist
+    {
+        public List<Break> Breaks = new List<Break>();
+        public XmlNodeList ListadeXMl(XmlDocument doc,String no)
+        {
+            XmlNodeList listadenos = doc.SelectNodes(no);
+            return listadenos;
+        }
+        public XmlDocument XmlDocumento(String caminho) {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);//Super necessario
+            XmlDocument doc = new XmlDocument();
+            doc.Load(caminho);
+            return doc;
+        }
+
+        public void setBreaks(List <XmlNode>list)
+        {
+
+            /*foreach (var item in listIns)
+            {
+                Ins objins = new Ins();
+
+                string[] attributeNames = { "Id", "CTA", "Source", "PtLoc", "MovedTo", "Type", "Title", "File", "Folder", "Text", "Checked", "Err", "sErr", "HoraAudio", "HoraPK", "IsAudioFile", "DurOrig", "Dur", "Refr", "DurRefr", "PtVh", "PtMx", "MxIni", "Intro", "Vol", "Bitrate", "Reg", "MD5" };
+
+                foreach (string attributeName in attributeNames)
+                {
+                    string attributeValue = item.Attributes[attributeName]?.Value;
+                    if (string.IsNullOrEmpty(attributeValue))
+                    {
+                        attributeValue = "Null";
+                    }
+                    typeof(Ins).GetProperty(attributeName).SetValue(objins, attributeValue);
+                }
+
+                objLista.Add(objins);
+
+            }*/
+
+
+
+
+            foreach (var item in list)
+            {
+                Break objBreak = new Break();
+                //Melhor maneira de fazer
+
+
+                string[] attributeNames = { "Ins", "Changed", "Locked", "Filetime", "Filetime2", "Time", "Type", "Id", "Id_Edited", "Fixo", "Descarte", "Net", "Dur", "Slots", "Orig" };
+
+                foreach (string attributeName in attributeNames)
+                {
+                    string attributeValue = item.Attributes[attributeName].Value;
+                    if (string.IsNullOrEmpty(attributeValue))
+                    {
+                        attributeValue = "Null";
+                    }
+                    typeof(Break).GetProperty(attributeName)?.SetValue(objBreak, attributeValue);
+                }
+
+                Breaks.Add(objBreak);
+
+
+                /*
+                Maneira grande de fazer
+                objBreak.Ins = item.Attributes["Ins"]?.Value;
+                objBreak.locked = item.Attributes["Locked"]?.Value;
+                objBreak.Changed = item.Attributes["Changed"]?.Value;
+                objBreak.Filetime = item.Attributes["Filetime"]?.Value;
+                objBreak.Filetime2 = item.Attributes["Filetime2"]?.Value;
+                objBreak.Time = item.Attributes["Time"]?.Value;
+                objBreak.Type = item.Attributes["Type"]?.Value;
+                objBreak.Id = item.Attributes["Id"]?.Value;
+                objBreak.Id_Edited = item.Attributes["Id_Edited"]?.Value;
+                objBreak.Fixo = item.Attributes["Fixo"]?.Value;
+                objBreak.Descarte = item.Attributes["Descarte"]?.Value;
+                objBreak.Net = item.Attributes["Net"]?.Value;
+                objBreak.Dur = item.Attributes["Dur"]?.Value;
+                objBreak.Slots = item.Attributes["Slots"]?.Value;
+                objBreak.Orig = item.Attributes["Orig"]?.Value;
+                */
+
+            }
+
+
+        }
+
+        public List<XmlNode> GetAllIns(List<XmlNode> list)
+        {
+            List<XmlNode> listains = new List<XmlNode>();
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list.Count > 0)
+                {
+                    foreach (XmlNode childNode in list[i].ChildNodes)
+                    {
+                        if (childNode.Name.StartsWith("Ins"))
+                        {
+                            listains.Add(childNode);
+                        }
+                    }
+                }
+            }
+
+            return listains;
+        }
+
+        public List<string> TransformarEmAtributos(XmlNode node) {
+
+
+            List<string> nomesAtributos = new List<string>();
+
+            foreach (XmlAttribute attribute in node.Attributes)
+            {
+                string nomeAtributo = attribute.Name;
+                nomesAtributos.Add(nomeAtributo);
+            }
+
+        
+        
+             return nomesAtributos;
+        }
+
+        
+
+
+
+    }
+}
